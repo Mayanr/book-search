eac722537a60b9d132d004b92184a95bdbdb778b
Mayan Annie Rothstein <mayan@bu.edu>
2019-08-05 18:55:40 -0700

    Updates

---
+++ precache-manifest.5559f604420babbfdfa52e3425316a9a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9a85e967a8394b25b20a96a972b69c1",
+    "url": "/book-search/index.html"
+  },
+  {
+    "revision": "cb2fd7147b57061f67f5",
+    "url": "/book-search/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "e782fb689ed5a7936905",
+    "url": "/book-search/static/css/main.f01b2234.chunk.css"
+  },
+  {
+    "revision": "cb2fd7147b57061f67f5",
+    "url": "/book-search/static/js/2.6469986b.chunk.js"
+  },
+  {
+    "revision": "e782fb689ed5a7936905",
+    "url": "/book-search/static/js/main.3f7e8662.chunk.js"
+  },
+  {
+    "revision": "925f42def18f4b184d6c",
+    "url": "/book-search/static/js/runtime~main.31a83aef.js"
+  },
+  {
+    "revision": "23d0d78450a152fa0aa7ec46f0413807",
+    "url": "/book-search/static/media/magnifyBooks.23d0d784.png"
+  }
+]);
